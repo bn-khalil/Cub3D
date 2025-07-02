a0216b7a8ad4d46ad40f87754aa64a9a95337cb6
Soufiane <sofiansofianait@gmail.com>
2025-07-02 15:45:24 +0000

    handling some norms in the engine

---
+++ src/utils/memory_managemnt.d
@@ -0,0 +1,2 @@
+src/utils/memory_managemnt.o: src/utils/memory_managemnt.c \
+ src/utils/../../inc/cub3d.h /home/sait-nac/.local/include/mlx.h
---
+++ src/utils/ray_utils.d
@@ -0,0 +1,2 @@
+src/utils/ray_utils.o: src/utils/ray_utils.c src/utils/../../inc/cub3d.h \
+ /home/sait-nac/.local/include/mlx.h
