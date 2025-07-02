4398a1f3337058d4469555646ac346073c2e626b
bn-khalil <benkhaliltouda3201@gmail.com>
2025-07-02 20:21:04 +0100

    add sprite checker

--- src/main.c
+++ src/main.c
@@ -14,6 +14,25 @@ void	is_file_ext_valid(char *filename)
 		ft_error("invalid file name!\n", NULL);
 }
 
+void check_fd(t_container *content, char *path)
+{
+	int fd;
+
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		ft_error("loading sprite failed!\n", content);
+	else
+		close(fd);
+}
+void check_sprite_files(t_container *content)
+{
+	check_fd(content, "./textures/h1.xpm");
+	check_fd(content, "./textures/h2.xpm");
+	check_fd(content, "./textures/h3+.xpm");
+	check_fd(content, "./textures/h4.xpm");
+	check_fd(content, "./textures/h5.xpm");
+	check_fd(content, "./textures/h6.xpm");
+}
 void f()
 {
 	system("leaks cub3d; lsof -c cub3d ");
@@ -21,13 +40,14 @@ void f()
 int	main(int ac, char **av)
 {
 	t_container	content;
-	atexit(f);
+	// atexit(f);
 	if (ac != 2)
 		return (write(2, "Invalid number of arrguments!\n", 31), 1);
 	is_file_ext_valid(av[1]);
 	ft_memset(&content, 0, sizeof(t_container));
 	content.filename = av[1];
 	get_and_init_map(&content);
+	check_sprite_files(&content);
 	start_the_play(&content);
 	ft_free_all(&content);
 	return (0);
